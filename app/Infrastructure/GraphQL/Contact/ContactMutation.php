265ca35c7757fce99e7c32a57cd56e6397bb78cc
Martin Obst <maobst@seznam.cz>
2025-07-28 22:17:37 +0200

    feat: add resolvers for mutations

--- app/Infrastructure/GraphQL/Contact/ContactMutation.php
+++ app/Infrastructure/GraphQL/Contact/ContactMutation.php
@@ -1,22 +1,23 @@
-<?php declare(strict_types=1);
+<?php declare(strict_types = 1);
 
-namespace App\Presentation\GraphQL\Contact;
+namespace App\Infrastructure\GraphQL\Contact;
 
 
-use App\Application\UseCase\Contact\CreateContactUseCase\CreateContactUseCase;
 use App\Application\UseCase\Contact\CreateContactUseCase\CreateContactUseCaseInput;
-use App\Application\UseCase\Contact\UpdateContactUseCase\UpdateContactUseCase;
 use App\Application\UseCase\Contact\UpdateContactUseCase\UpdateContactUseCaseInput;
+use App\Infrastructure\GraphQL\Resolver\Mutation\CreateContactMutationResolver;
+use App\Infrastructure\GraphQL\Resolver\Mutation\UpdateContactMutationResolver;
 use GraphQL\Type\Definition\ObjectType;
 use GraphQL\Type\Definition\Type;
 
 final class ContactMutation extends ObjectType
 {
 	public function __construct(
-		private readonly CreateContactUseCase $createContactEndpoint,
-		private readonly UpdateContactUseCase $updateContactEndpoint,
-		private readonly ContactType          $contactType
-	) {
+		private readonly CreateContactMutationResolver $createContactMutationResolver,
+		private readonly UpdateContactMutationResolver $updateContactMutationResolver,
+		private readonly ContactType                   $contactType
+	)
+	{
 		parent::__construct([
 			'name' => 'Mutation',
 			'fields' => [
@@ -25,13 +26,13 @@ final class ContactMutation extends ObjectType
 					'args' => [
 						'firstName' => Type::nonNull(Type::string()),
 						'lastName' => Type::nonNull(Type::string()),
-						'phone' => Type::nonNull(Type::string())
+						'phone' => Type::nonNull(Type::string()),
 					],
-					'resolve' => fn ($root, array $args) => $this->createContactEndpoint->process(new CreateContactUseCaseInput(
+					'resolve' => fn($root, array $args) => $this->createContactMutationResolver->resolve(new CreateContactUseCaseInput(
 						firstName: $args['firstName'],
 						lastName: $args['lastName'],
 						phone: $args['phone']
-					))
+					)),
 				],
 				'updateContact' => [
 					'type' => $this->contactType,
@@ -39,16 +40,16 @@ final class ContactMutation extends ObjectType
 						'id' => Type::nonNull(Type::id()),
 						'firstName' => Type::string(),
 						'lastName' => Type::string(),
-						'phone' => Type::string()
+						'phone' => Type::string(),
 					],
-					'resolve' => fn ($root, array $args) => $this->updateContactEndpoint->process(new UpdateContactUseCaseInput(
-						id: $args['id'],
+					'resolve' => fn($root, array $args) => $this->updateContactMutationResolver->resolve(new UpdateContactUseCaseInput(
+						id: (int) $args['id'],
 						firstName: $args['firstName'],
 						lastName: $args['lastName'],
 						phone: $args['phone']
-					))
-				]
-			]
+					)),
+				],
+			],
 		]);
 	}
 }
