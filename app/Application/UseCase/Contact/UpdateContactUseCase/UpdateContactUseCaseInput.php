d050b380b56cd96ed79c20bef78bf4823d926078
Martin Obst <maobst@seznam.cz>
2025-07-29 21:38:43 +0200

    fix: remove + from phone format

--- app/Application/UseCase/Contact/UpdateContactUseCase/UpdateContactUseCaseInput.php
+++ app/Application/UseCase/Contact/UpdateContactUseCase/UpdateContactUseCaseInput.php
@@ -10,7 +10,7 @@ final readonly class UpdateContactUseCaseInput
 {
 	public function __construct(public int $id, public string $firstName, public string $lastName, public string $phone)
 	{
-		if (!preg_match('/^\+[1-9]\d{1,14}$/', $this->phone)) {
+		if (!preg_match('/^[1-9]\d{1,14}$/', $this->phone)) {
 			throw new InvalidArgumentException('Invalid phone number format. Must follow E.164 format.');
 		}
 	}
